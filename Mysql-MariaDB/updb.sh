3b1d3eab0c2b6acbbe5241fbeeea4b2b4aac8228
Daelerin <daelerin@daelerin.fr>
2024-03-21 16:42:58 +0100

    ajout de comentaire

--- Mysql-MariaDB/updb.sh
+++ Mysql-MariaDB/updb.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
+# Demandez à l'utilisateur le chemin de la sauvegarde
 echo -n "Entrez le chemin vers votre backup"
 read CHEMIN
 
-latest_backup=$(ls -lrt $CHEMIN | grep ".tar.gz" | tail -n 1 | cut -d ":" -f 2 | cut -d " " -f 2);
+# Extrayez le fichier .tar.gz le plus récent dans le répertoire racine
+latest_backup=$(ls -lrt $CHEMIN | grep ".tar.gz" | tail -n 1 | cut -d ":" -f 2 | cut -d " " -f 2)
 tar -zxvf $CHEMIN/$latest_backup -C /
 
+# Demandez à l'utilisateur son nom d'utilisateur, son mot de passe et le nom de sa base de données
 echo -n "Entrez le nom de votre utilisateur : "
 read user
 echo -n "Entrez le mot de passe pour l'utilisateur $user"
@@ -12,10 +16,7 @@ read -s passwd
 echo -n "Entrez la basse de donnée : "
 read dbname
 
-latest_db=$(ls -lrt $CHEMIN | grep ".sql.gz" | tail -n 1 | cut -d ":" -f 2 | cut -d " " -f 2);
+# Importez le fichier .sql le plus récent dans la base de données spécifiée
+latest_db=$(ls -lrt $CHEMIN | grep ".sql.gz" | tail -n 1 | cut -d ":" -f 2 | cut -d " " -f 2)
 gunzip $CHEMIN/$latest_db
-echo $latest_db
-
-latest_sql=$(ls -lrt $CHEMIN | grep ".sql" | tail -n 1 | cut -d ":" -f 2 | cut -d " " -f 2);
-mysql $user $mdp $bdd < $CHEMIN/$latest_sql
-echo $latest_sql
+mysql $user $passwd $dbname < $CHEMIN/$(ls -lrt $CHEMIN | grep ".sql" | tail -n 1 | cut -d ":" -f 2 | cut -d " " -f 2)
