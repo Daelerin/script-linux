6d33a292a9e721c2126e5eedb99ee8e39e33d524
Daelerin <64404386+Daelerin@users.noreply.github.com>
2024-09-19 10:52:11 +0200

    Update install-ws.sh

--- withsecure/install-ws.sh
+++ withsecure/install-ws.sh
@@ -11,5 +11,5 @@ dpkg -i /tmp/linuxsecurity.deb
 # Si la distribution est trop récente cela peut généré une erreur il y a cependant un moyen de coutournement par exemple 
 # mon serveur est en debian et with secure n'est pas encore compatible avec cette version il suffit d'ajouter cete option a la commande : --override-distro debian:11
 # permetant ainsi de dire au script d'activation que vous avez un debian 11 et non un debian 12
-subkey = <vorteclé>
+subkey="<vorteclé>"
 /opt/f-secure/linuxsecurity/bin/activate --psb --subscription-key  $subkey
