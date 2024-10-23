d78dab6b572b075c405fc864d356ee0cca12bf6e
Daelerin <64404386+Daelerin@users.noreply.github.com>
2024-10-23 08:31:27 +0000

    ajout des dépots

--- withsecure/instal-EVM.sh
+++ withsecure/instal-EVM.sh
@@ -2,13 +2,15 @@
 apt update 
 echo "installation des dépendances"
 
+version_id=$(grep VERSION_ID /etc/os-release | cut -d '=' -f 2- | tr -d '"')
+
 apt install -y libx11-xcb1 libxrandr2 libasound2 libpangocairo-1.0-0 libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libnss3 libxss1 libffi6 libssl1.1
-wget -O /tmp/powershell_7.4.5-1.deb https://github.com/PowerShell/PowerShell/releases/download/v7.4.5/powershell_7.4.5-1.deb_amd64.deb
+wget -Q -O /tmm/packages-microsoft-prod.deb https://packages.microsoft.com/config/debian/$version_id/packages-microsoft-prod.deb
 wget -O /tmp/libpython2.7-stdlib_2.7.16-2+deb10u1_amd64.deb http://ftp.de.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.16-2+deb10u1_amd64.deb
 wget -O /tmp/python2.7-minimal_2.7.16-2+deb10u1_amd64.deb http://ftp.de.debian.org/debian/pool/main/p/python2.7/python2.7-minimal_2.7.16-2+deb10u1_amd64.deb
 wget -O /tmp/python2.7_2.7.18-8+deb11u1_amd64.deb http://ftp.de.debian.org/debian/pool/main/p/python2.7/python2.7_2.7.18-8+deb11u1_amd64.deb
 
-apt install /tmp/powershell_7.4.5-1.deb
+dpkg -i /tmp/packages-microsoft-prod.deb
 apt install /tmp/libpython2.7-stdlib_2.7.16-2+deb10u1_amd64.deb
 apt install /tmp/python2.7-minimal_2.7.16-2+deb10u1_amd64.deb
 apt install /tmp/python2.7_2.7.18-8+deb11u1_amd64.deb
