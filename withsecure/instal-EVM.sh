f4d1028a1e017ae2983eb31a52750d65860436df
Daelerin <64404386+Daelerin@users.noreply.github.com>
2024-10-24 07:40:43 +0000

    Ca marche pas normal, ça n'a pas de pieds

--- withsecure/instal-EVM.sh
+++ withsecure/instal-EVM.sh
@@ -4,7 +4,7 @@ echo "installation des dépendances"
 
 version_id=$(grep VERSION_ID /etc/os-release | cut -d '=' -f 2- | tr -d '"')
 
-apt install -y libx11-xcb1 libxrandr2 libasound2 libpangocairo-1.0-0 libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libnss3 libxss1 libssl1.1
+apt install -y libx11-xcb1 libxrandr2 libasound2 libpangocairo-1.0-0 libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libnss3 libxss1 libssl1.1 libreadline7
 wget -O /tmp/packages-microsft-prod.deb https://packages.microsoft.com/config/debian/$version_id/packages-microsoft-prod.deb
 wget -O /tmp/libffi6.deb http://ftp.de.debian.org/debian/pool/main/libf/libffi/libffi6_3.2.1-9_amd64.deb
 wget -O /tmp/libpython2.7-stdlib_2.7.16-2+deb10u1_amd64.deb http://ftp.de.debian.org/debian/pool/main/p/python2.7/libpython2.7-stdlib_2.7.16-2+deb10u1_amd64.deb
@@ -18,7 +18,6 @@ apt install /tmp/libffi6.deb
 apt install /tmp/libpython2.7-stdlib_2.7.16-2+deb10u1_amd64.deb
 apt install /tmp/python2.7-minimal_2.7.16-2+deb10u1_amd64.deb
 apt install /tmp/python2.7_2.7.18-8+deb11u1_amd64.deb
-
 
 echo "Récupération de l'installeur"
 wget -O /tmp/installer.deb https://updates-api.radar-prd.fsapi.com/api/1.1/ProductUpdates/Components/ScanNodeAgent/Releases/4.0.0.0/Download
