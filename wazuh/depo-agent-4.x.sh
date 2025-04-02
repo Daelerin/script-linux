5ce2439fc5a4c0ffb638bfc72133437f04543dbd
Daelerin <64404386+Daelerin@users.noreply.github.com>
2025-04-02 08:30:21 +0000

    oui

---
+++ wazuh/depo-agent-4.x.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
+echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+apt update && apt upgrade -y wazuh-agent 
